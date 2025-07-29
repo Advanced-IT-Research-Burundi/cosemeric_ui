21f9c25b2b9447d7eb48d58f5b069cb378dd65fe
Jean Lionel <nijeanlionel@gmail.com>
2025-07-29 11:51:18 +0200

    implementation of printing , export to excel file

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-    <h1>BONJOUR LE MONDE</h1>
+    <button @click="printPage">Imprimer </button>
+    <button @click="generatePdfA">Generate PDF  </button>
+    <button @click="generateExcel"> Generate Excel File  </button>
+    <div id="bonjour">
+        <h1>BONJOUR LE MONDE</h1>
+    </div>
+    
   </div>
 </template>
 
-<script>
-import AppLayout from '../components/layout/AppLayout.vue';
+<script setup >
+import usePrint from '../utils/usePrint';
+const {print , generatePdf , exportToExcel } = usePrint();
 
-export default {
-  name: 'Dashboard',
-  components: {
-    AppLayout
-  }
+function printPage(){
+  print("bonjour")
 }
+function generatePdfA(){
+  generatePdf("bonjour")
+}
+
+function generateExcel(){
+  exportToExcel(['A', 'B', 'C']);
+}
+
+
 </script>
