03cdb97515c7c6d8474aa06040729e8776a40a36
Jean Lionel <nijeanlionel@gmail.com>
2025-12-06 16:45:41 +0200

    remboursement

--- src/views/remboursements/Remboursements.vue
+++ src/views/remboursements/Remboursements.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container py-4 px-4">
+  <div class="">
     <div class="d-flex justify-content-between align-items-center mb-4">
       <h2 class="mb-0">Gestion des Remboursements</h2>
       <router-link to="/remboursements/add" class="btn btn-primary">
